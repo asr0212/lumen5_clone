7dd203b79142c0473a8f52796baecbae6cc1dd4e
asr0212 <anshumansinghm@gmail.com>
2022-04-03 15:01:47 +0530

    final update

---
+++ index.js
@@ -0,0 +1,43 @@
+document.querySelector("#logoPoint").addEventListener("click",function(){
+  window.location.href = "index.html";
+});
+
+document.querySelector("#clickPricing").addEventListener("click",function(){
+ window.location.href = "pricing.html";
+});
+
+document.querySelector("#clickEnterprise").addEventListener("click",function(){
+    window.location.href = "enterprise.html";
+});
+
+document.querySelector("#clickCase").addEventListener("click",function(){
+    window.location.href = "case.html";
+});
+document.querySelector("#login").addEventListener("click",function(){
+   window.location.href = "login.html";
+});
+document.querySelector("#signup").addEventListener("click",function(){
+    window.location.href = "signup.html";
+ });
+ document.querySelector("#signPoint").addEventListener("click",function(){
+    window.location.href = "signup.html";
+ });
+ document.querySelector(".container-signup button").addEventListener("click",function(){
+    window.location.href = "signup.html";
+ });
+ document.querySelector("#featurePoint").addEventListener("click",function(){
+    window.location.href = "features.html";
+ });
+ document.querySelector("#clickLearn").addEventListener("click",function(){
+    window.location.href = "blog.html";
+ });
+ document.querySelector("#aboutlogin").addEventListener("click",function(){
+    window.location.href = "login.html";
+ });
+ document.querySelector("#aboutSign").addEventListener("click",function(){
+    window.location.href = "signup.html";
+ });
+
+ document.querySelector("#view").addEventListener("click",function(){
+    window.location.href = "viewCareer.html";
+ });
---
+++ videotoolpages/vt.js
@@ -0,0 +1,43 @@
+document.querySelector("#logoPoint").addEventListener("click",function(){
+    window.location.href = "/lumen5_clone/index.html";
+  });
+  
+  document.querySelector("#clickPricing").addEventListener("click",function(){
+   window.location.href = "/lumen5_clone/pricing.html";
+  });
+  
+  document.querySelector("#clickEnterprise").addEventListener("click",function(){
+      window.location.href = "/lumen5_clone/enterprise.html";
+  });
+  
+  document.querySelector("#clickCase").addEventListener("click",function(){
+      window.location.href = "/lumen5_clone/case.html";
+  });
+  document.querySelector("#login").addEventListener("click",function(){
+     window.location.href = "/lumen5_clone/login.html";
+  });
+  document.querySelector("#signup").addEventListener("click",function(){
+      window.location.href = "/lumen5_clone/signup.html";
+   });
+   document.querySelector("#signPoint").addEventListener("click",function(){
+      window.location.href = "/lumen5_clone/signup.html";
+   });
+   document.querySelector(".container-signup button").addEventListener("click",function(){
+      window.location.href = "/lumen5_clone/signup.html";
+   });
+   document.querySelector("#featurePoint").addEventListener("click",function(){
+      window.location.href = "/lumen5_clone/features.html";
+   });
+   document.querySelector("#clickLearn").addEventListener("click",function(){
+      window.location.href = "/lumen5_clone/blog.html";
+   });
+   document.querySelector("#aboutlogin").addEventListener("click",function(){
+      window.location.href = "/lumen5_clone/login.html";
+   });
+   document.querySelector("#aboutSign").addEventListener("click",function(){
+      window.location.href = "/lumen5_clone/signup.html";
+   });
+  
+   document.querySelector("#view").addEventListener("click",function(){
+      window.location.href = "/lumen5_clone/viewCareer.html";
+   });
